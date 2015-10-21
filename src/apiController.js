aacad9d39c2ab8919812194ccf0e31f30c936277
Yami <yamikuronue@gmail.com>
2015-10-21 19:52:34 -0400

    Add routes for user retrieval
    Involved with #39

--- src/apiController.js
+++ src/apiController.js
@@ -1,6 +1,14 @@
 'use strict';
-
-const dao = require('./dao.js'); //This can be renamed later
+/**
+ * The controller for the API
+ *
+ *
+ * @module apiController
+ * @license MIT
+ * @author yamikuronue
+ */
+ 
+const dao = require('./dao.js');
 
 const controller = {
 	getAllGames: (res) => {
@@ -12,7 +20,7 @@ const controller = {
 			res.send(data);
 		}).catch((err) => {
 			//TODO: logging errors
-			res.status(500).send({error: `Database error: ${err}`});
+			res.status(500).send({error: err.toString()});
 		});
 	},
 
@@ -36,7 +44,7 @@ const controller = {
 			res.send(data);
 		}).catch((err) => {
 			//TODO: logging errors
-			res.status(500).send({error: `Database error: ${err}`});
+			res.status(500).send({error: err.toString()});
 		});
 	},
 
@@ -49,7 +57,7 @@ const controller = {
 			res.send(data);
 		}).catch((err) => {
 			//TODO: logging errors
-			res.status(500).send({error: `Database error: ${err}`});
+			res.status(500).send({error: err.toString()});
 		});
 	},
 
@@ -73,8 +81,55 @@ const controller = {
 			res.send(data);
 		}).catch((err) => {
 			//TODO: logging errors
-			res.status(500).send({error: `Database error: ${err}`});
+			res.status(500).send({error: err.toString()});
 		});
+	},
+	
+	getAllUsers: (res) => {
+		return dao.getAllUsers().then((data) => {
+			for (let i = 0; i < data.length; i++) {
+				data[i].canonical = `/api/user/${data[i].id}`;
+			}
+			res.send(data);
+		}).catch((err) => {
+			//TODO: logging errors
+			res.status(500).send({error: err.toString()});
+		});
+	},
+	
+	getUser: (req, res) => {
+		
+		const handleData = (data) => {
+			if (Array.isArray(data)) {
+				data = data[0]; //Only the first game
+			}
+
+			if (!data) {
+				res.status(404);
+				return;
+			}
+			data.canonical = `/api/user/${data.id}`;
+
+			res.send(data);
+		};
+			
+		const handleError = (err) => {
+			//TODO: logging errors
+			res.status(500).send({error: err.toString()});
+		};
+						
+		//Check if the ID is a number
+		if (Number.parseInt(req.params.id, 10) == req.params.id) { //eslint-disable-line eqeqeq
+			return dao.getUser(req.params.id).then(handleData).catch(handleError);
+		
+		//Otherwise it's a name
+		} else if (req.params.id) {
+			return dao.getUserByName(req.params.id).then(handleData).catch(handleError);
+		}
+	
+		//Fallthrough if we did neither:
+		res.status(501).send({error: 'Missing ID'});
+		return Promise.resolve();
 	}
 };
 
--- test/unit/apiTests.js
+++ test/unit/apiTests.js
@@ -59,7 +59,7 @@ describe('Game API controller', () => {
 		it('should respond with an error when the DB errors', () => {
 
 			const expected = {
-				error: 'Database error: Error: oops i asploded'
+				error: 'Error: oops i asploded'
 			};
 
 			sandbox.stub(dao, 'getAllGames').rejects('oops i asploded');
@@ -115,11 +115,10 @@ describe('Game API controller', () => {
 		it('should respond with an error when the DB errors', () => {
 
 			const expected = {
-				error: 'Database error: Error: oops i asploded'
+				error: 'Error: oops i asploded'
 			};
 
 			sandbox.stub(dao, 'getAllBoards').rejects('oops i asploded');
-			const start = new Date();
 
 			const mockResponse = {
 				status: (code) => {
@@ -224,8 +223,30 @@ describe('Game API controller', () => {
 					assert.equal(404, code, 'Should return a 404 if anything');
 					return mockResponse;
 				},
-				send: (response) => {
+				send: () => {
 					assert.notOk(true, 'Should not send data');
+					return mockResponse;
+				}
+			};
+
+			return api.getGame(mockRequest, mockResponse);
+		});
+		
+		it('should return a 501 if no ID passed in', () => {
+			sandbox.stub(dao, 'getGame').resolves(undefined);
+
+			const mockRequest = {
+				params: {
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(501, code, 'Should return a 501');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.deepEqual(response, {error: 'Missing ID'});
 					return mockResponse;
 				}
 			};
@@ -236,7 +257,7 @@ describe('Game API controller', () => {
 		it('should respond with an error when the DB errors', () => {
 
 			const expected = {
-				error: 'Database error: Error: oops i asploded'
+				error: 'Error: oops i asploded'
 			};
 
 			sandbox.stub(dao, 'getGame').rejects('oops i asploded');
@@ -351,8 +372,30 @@ describe('Game API controller', () => {
 					assert.equal(404, code, 'Should return a 404 if anything');
 					return mockResponse;
 				},
-				send: (response) => {
+				send: () => {
 					assert.notOk(true, 'Should not send data');
+					return mockResponse;
+				}
+			};
+
+			return api.getBoard(mockRequest, mockResponse);
+		});
+		
+		it('should return a 501 if no ID passed in', () => {
+			sandbox.stub(dao, 'getBoard').resolves(undefined);
+
+			const mockRequest = {
+				params: {
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(501, code, 'Should return a 501');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.deepEqual(response, {error: 'Missing ID'});
 					return mockResponse;
 				}
 			};
@@ -363,7 +406,7 @@ describe('Game API controller', () => {
 		it('should respond with an error when the DB errors', () => {
 
 			const expected = {
-				error: 'Database error: Error: oops i asploded'
+				error: 'Error: oops i asploded'
 			};
 
 			sandbox.stub(dao, 'getBoard').rejects('oops i asploded');
@@ -387,6 +430,245 @@ describe('Game API controller', () => {
 			};
 
 			return api.getBoard(mockRequest, mockResponse);
+		});
+	});
+});
+
+describe('User API Controller', () => {
+	let sandbox;
+
+	before(() => {
+	});
+
+	beforeEach(() => {
+		sandbox = Sinon.sandbox;
+	});
+
+	afterEach( () => {
+		sandbox.restore();
+	});
+	
+	describe('/api/users', () => {
+		it('should return a list of users', () => {
+			const data = [{
+				ID: '1',
+				Name: 'user1',
+				Admin: true
+			}, {
+				ID: '2',
+				Name: 'user2',
+				Admin: false
+			}];
+
+			sandbox.stub(dao, 'getAllUsers').resolves(data);
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(200, code, 'Should return a 200 ok if anything');
+					return mockResponse;
+				},
+				send: (response) => {
+					data[0].canonical = 'api/users/1';
+					data[1].canonical = 'api/users/2';
+					assert.deepEqual(data, response);
+					return mockResponse;
+				}
+			};
+
+			return api.getAllUsers(mockResponse);
+		});
+
+		it('should respond with an error when the DB errors', () => {
+
+			const expected = {
+				error: 'Error: oops i asploded'
+			};
+
+			sandbox.stub(dao, 'getAllUsers').rejects('oops i asploded');
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(500, code, 'Should return a 500 error');
+					return mockResponse;
+				}, send: (response) => {
+					assert.ok(response);
+					assert.deepEqual(expected, response);
+					return mockResponse;
+				}
+			};
+
+			return api.getAllUsers(mockResponse);
+		});
+	});
+	
+	describe('/api/user/{ID}', () => {
+		it('should return a user if one exists by ID', () => {
+			const data = {
+				ID: '1',
+				Name: 'user1',
+				Admin: true
+			};
+
+			sandbox.stub(dao, 'getUser').resolves(data);
+
+			const mockRequest = {
+				params: {
+					id: 1
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(200, code, 'Should return a 200 ok if anything');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.isTrue(dao.getUser.called, 'Should call the user search by id');
+					data.canonical = 'api/users/1';
+					assert.deepEqual(data, response);
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
+		});
+		
+		it('should return a user if one exists by name', () => {
+			const data = {
+				ID: '1',
+				Name: 'user1',
+				Admin: true
+			};
+
+			sandbox.stub(dao, 'getUserByName').resolves(data);
+
+			const mockRequest = {
+				params: {
+					id: 'user1'
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(200, code, 'Should return a 200 ok if anything');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.isTrue(dao.getUserByName.called, 'Should call the user search by name');
+					data.canonical = 'api/users/1';
+					assert.deepEqual(data, response);
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
+		});
+
+		it('should return only the first user if more than one exists', () => {
+			const data = [{
+				ID: '1',
+				Name: 'user1',
+				Admin: true
+			}, {
+				ID: '2',
+				Name: 'user2',
+				Admin: false
+			}];
+
+			sandbox.stub(dao, 'getUser').resolves(data);
+
+			const mockRequest = {
+				params: {
+					id: 1
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(200, code, 'Should return a 200 ok if anything');
+					return mockResponse;
+				},
+				send: (response) => {
+					data[0].canonical = '/users/1';
+					assert.deepEqual(data[0], response);
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
+		});
+
+		it('should return a 404 if no such user exists', () => {
+			sandbox.stub(dao, 'getUser').resolves(undefined);
+
+			const mockRequest = {
+				params: {
+					id: 1
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(404, code, 'Should return a 404 if anything');
+					return mockResponse;
+				},
+				send: () => {
+					assert.notOk(true, 'Should not send data');
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
+		});
+		
+		it('should return a 501 if no ID passed in', () => {
+			sandbox.stub(dao, 'getUser').resolves(undefined);
+
+			const mockRequest = {
+				params: {
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(501, code, 'Should return a 501');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.deepEqual(response, {error: 'Missing ID'});
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
+		});
+
+		it('should respond with an error when the DB errors', () => {
+
+			const expected = {
+				error: 'Error: oops i asploded'
+			};
+
+			sandbox.stub(dao, 'getUser').rejects('oops i asploded');
+
+			const mockRequest = {
+				params: {
+					id: 1
+				}
+			};
+
+			const mockResponse = {
+				status: (code) => {
+					assert.equal(500, code, 'Should return a 500 error');
+					return mockResponse;
+				},
+				send: (response) => {
+					assert.ok(response);
+					assert.deepEqual(expected, response);
+					return mockResponse;
+				}
+			};
+
+			return api.getUser(mockRequest, mockResponse);
 		});
 	});
 });
