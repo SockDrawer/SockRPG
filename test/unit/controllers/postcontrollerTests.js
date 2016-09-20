44bd646bf2de8d3ed7074c2f29ae3e888e2bf598
Yami <yamikuronue@gmail.com>
2016-09-20 19:10:11 +0000

    Feature: post controller, with retrieve-by-id and insert capabilities

---
+++ test/unit/controllers/postcontrollerTests.js
@@ -0,0 +1,137 @@
+'use strict';
+const Chai = require('chai');
+Chai.should();
+
+const Sinon = require('sinon');
+require('sinon-as-promised');
+const sinonChai = require('sinon-chai');
+Chai.use(sinonChai);
+
+const postController = require('../../../src/controllers/postController.js');
+
+const Game = require('../../../src/model/Game');
+const Board = require('../../../src/model/Board');
+const Thread = require('../../../src/model/Thread');
+const Post = require('../../../src/model/Post');
+
+describe('Post API Controller', () => {
+	let sandbox, mockRequest, mockResponse;
+
+
+	before(() => {
+	});
+
+	beforeEach(() => {
+		sandbox = Sinon.sandbox;
+		mockResponse = {};
+		mockResponse.status = sandbox.stub().returns(mockResponse);
+		mockResponse.send = sandbox.stub().returns(mockResponse);
+		mockResponse.end = sandbox.stub().returns(mockResponse);
+	});
+
+	afterEach( () => {
+		sandbox.restore();
+	});
+
+
+	describe('GET /threads/{ID}/posts', () => {
+
+		mockRequest = {
+			params: {
+				id: 1
+			}
+		};
+		
+		it('Should return 404 if no such thread', () => {
+		//	sandbox.stub(Thread, 'getThread').resolves(null);
+		//	return postController.getPosts(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
+		});
+		
+		it('Should return an empty list if the thread has no posts', () => {
+			
+		});
+		
+		it('Should return a list of posts if there are any', () => {
+			
+		});
+	});
+	
+	describe('POST /thread/{ID}/posts', () => {
+
+		before(() => {
+			mockRequest = {
+				params: {
+					id: 3
+				},
+				body: {
+					Body: 'wake up where the clouds are far behind me'
+				}
+			};
+		});
+		
+		it('Should return 404 if no such thread', () => {
+			sandbox.stub(Thread, 'getThread').resolves(null);
+			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
+		});
+		
+		it('Should add a post', () => {
+			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
+			sandbox.stub(Post, 'addPost').resolves([10]);
+			
+			return postController.addPost(mockRequest, mockResponse).then(() => Post.addPost.should.have.been.calledWith(mockRequest.body));
+		});
+		
+		it('Should return 200 if a post adds successfully', () => {
+			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
+			sandbox.stub(Post, 'addPost').resolves([10]);
+			
+			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(200));
+		});
+		
+		it('Should return ID of added post', () => {
+			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
+			sandbox.stub(Post, 'addPost').resolves([10]);
+			const expected = JSON.stringify({
+				id: 10
+			});
+			
+			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.send.should.have.been.calledWith(expected));
+		});
+	});
+	
+	describe('GET /posts/{ID}', () => {
+		before(() => {
+			mockRequest = {
+				params: {
+					id: 100
+				}
+			};
+		});
+		
+		it('Should return 404 if no such post', () => {
+			sandbox.stub(Post, 'getPostByID').resolves(null);
+			return postController.getPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
+		});
+		
+		it('Should return posts if they exist', () => {
+			let postData = {
+				ID: 1, 
+				Body: 'Somewhere over the rainbow?'
+			};
+			
+			const post = new Post(postData);
+			postData = post.serialize(); //add derived attributes
+			
+			sandbox.stub(Post, 'getPostByID').resolves(post);
+
+			
+			
+			return postController.getPost(mockRequest, mockResponse).then(() => {
+				Post.getPostByID.should.have.been.calledWith(100);
+
+				mockResponse.status.should.have.been.calledWith(200);
+				mockResponse.send.should.have.been.calledWith(JSON.stringify(postData));
+			});
+		});
+	});
+});
