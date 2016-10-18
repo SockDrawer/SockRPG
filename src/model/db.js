c6522825463ebe2abefdf26a20de6ab6dcde6c5e
Yami <yamikuronue@gmail.com>
2016-10-18 18:29:28 +0000

    Fix: Allow for restarting app when schema exists. Closes #89

--- src/model/db.js
+++ src/model/db.js
@@ -16,45 +16,68 @@ const db = {
 			connection: {
 				filename: config.database.filename
 			},
-		//	debug: true,
 			useNullAsDefault: true
 		});
 		
-		return knex.schema.createTableIfNotExists('Games', (table) => {
-			table.increments('ID').primary();
-			table.string('gameDescription');
-		}).then(() => {
-			return knex.schema.createTableIfNotExists('Users', (table) => {
-				table.increments('ID').primary();
-				table.string('Username').notNullable().unique();
-			});
-		}).then(() => {
-			return knex.schema.createTableIfNotExists('Boards', (table) => {
-				table.increments('ID').primary();
-				table.integer('Owner').references('Users.ID');//.notNullable();  //This shouldn't be nullable, but we don't have users working yet
-				table.integer('GameID').references('Games.ID').nullable();
-				table.string('Name').notNullable();
-				table.boolean('Adult').defaultTo(false);
-				table.string('Description').notNullable().defaultTo('');
-			});
-		}).then(() => {
-			return knex.schema.createTableIfNotExists('ChildBoards', (table) => {
-				table.increments('ID').primary();
-				table.integer('ParentID').references('Boards.ID').notNullable();
-				table.integer('ChildID').references('Boards.ID').notNullable();
-			});
-		}).then(() => {
-			return knex.schema.createTableIfNotExists('Threads', (table) => {
-				table.increments('ID').primary();
-				table.string('Title').notNullable();
-				table.integer('Board').references('Boards.ID').notNullable();
-			});
-		}).then(() => {
-			return knex.schema.createTableIfNotExists('Posts', (table) => {
-				table.increments('ID').primary();
-				table.integer('Thread').references('Threads.ID').notNullable();
-				table.string('Body').notNullable();
-			});
+		return knex.schema.hasTable('Games').then((exists) => {
+			if (!exists) {
+				return knex.schema.createTable('Games', (table) => {
+					table.increments('ID').primary();
+					table.string('gameDescription');
+				});
+			}
+		})
+		.then(() => knex.schema.hasTable('Users'))
+		.then((exists) => {
+			if (!exists) {
+				return knex.schema.createTable('Users', (table) => {
+					table.increments('ID').primary();
+					table.string('Username').notNullable().unique();
+				});
+			}
+		})
+		.then(() => knex.schema.hasTable('Boards'))
+		.then((exists) => {
+			if (!exists) {
+				return knex.schema.createTableIfNotExists('Boards', (table) => {
+					table.increments('ID').primary();
+					table.integer('Owner').references('Users.ID');//.notNullable();  //This shouldn't be nullable, but we don't have users working yet
+					table.integer('GameID').references('Games.ID').nullable();
+					table.string('Name').notNullable();
+					table.boolean('Adult').defaultTo(false);
+					table.string('Description').notNullable().defaultTo('');
+				});
+			}
+		})
+		.then(() => knex.schema.hasTable('Boards'))
+		.then((exists) => {
+			if (!exists) {
+				return knex.schema.createTableIfNotExists('ChildBoards', (table) => {
+					table.increments('ID').primary();
+					table.integer('ParentID').references('Boards.ID').notNullable();
+					table.integer('ChildID').references('Boards.ID').notNullable();
+				});
+			}
+		})
+		.then(() => knex.schema.hasTable('Threads'))
+		.then((exists) => {
+			if (!exists) {
+				return knex.schema.createTableIfNotExists('Threads', (table) => {
+					table.increments('ID').primary();
+					table.string('Title').notNullable();
+					table.integer('Board').references('Boards.ID').notNullable();
+				});
+			}
+		})
+		.then(() => knex.schema.hasTable('Posts'))
+		.then((exists) => {
+			if (!exists) {
+				return knex.schema.createTableIfNotExists('Posts', (table) => {
+					table.increments('ID').primary();
+					table.integer('Thread').references('Threads.ID').notNullable();
+					table.string('Body').notNullable();
+				});
+			}
 		}).then(() => {
 			db.initialised = true;
 			return Promise.resolve(db.initialised);
