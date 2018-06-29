e87315d04f015b9da9c135fc85db76e46624e8be
Accalia Elementia <accalia@elementia.me>
2018-06-29 16:28:24 +0000

    chore(refactor): remove redundant CYA code

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -156,9 +156,6 @@ function getBoard(req, res) {
 	}
 
 	return Board.getBoard(req.params.id).then((data) => {
-		if (Array.isArray(data)) {
-			data = data[0]; //Only the first board
-		}
 		if (!data) {
 			res.status(404).end();
 			return undefined;
