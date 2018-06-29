4612643f381c6cbc9867a7ae0e971138068bc627
Accalia Elementia <accalia@elementia.me>
2018-06-29 12:38:01 +0000

    chore(tests): update postController tests

--- test/unit/controllers/postcontrollerTests.js
+++ test/unit/controllers/postcontrollerTests.js
@@ -38,21 +38,21 @@ describe('Post API Controller', () => {
 				id: 1
 			}
 		};
-		
+
 		it('Should return 404 if no such thread', () => {
 		//	sandbox.stub(Thread, 'getThread').resolves(null);
 		//	return postController.getPosts(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
 		});
-		
+
 		it('Should return an empty list if the thread has no posts', () => {
-			
+
 		});
-		
+
 		it('Should return a list of posts if there are any', () => {
-			
+
 		});
 	});
-	
+
 	describe('POST /thread/{ID}/posts', () => {
 
 		before(() => {
@@ -65,37 +65,55 @@ describe('Post API Controller', () => {
 				}
 			};
 		});
-		
+
 		it('Should return 404 if no such thread', () => {
 			sandbox.stub(Thread, 'getThread').resolves(null);
 			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
 		});
-		
+
 		it('Should add a post', () => {
 			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
 			sandbox.stub(Post, 'addPost').resolves([10]);
-			
+
 			return postController.addPost(mockRequest, mockResponse).then(() => Post.addPost.should.have.been.calledWith(mockRequest.body));
 		});
-		
+		it('Should add a post with no body', () => {
+			mockRequest.body = undefined;
+			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
+			sandbox.stub(Post, 'addPost').resolves([10]);
+
+			return postController.addPost(mockRequest, mockResponse)
+				.then(() => Post.addPost.should.have.been.calledWith({
+					Thread: 3
+				}));
+		});
+
 		it('Should return 200 if a post adds successfully', () => {
 			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
 			sandbox.stub(Post, 'addPost').resolves([10]);
-			
+
 			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(200));
 		});
-		
+
+		it('Should return 500 if a post rejects', () => {
+			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
+			sandbox.stub(Post, 'addPost').rejects(new Error('foobar'));
+
+			return postController.addPost(mockRequest, mockResponse)
+			.then(() => mockResponse.status.should.have.been.calledWith(500));
+		});
+
 		it('Should return ID of added post', () => {
 			sandbox.stub(Thread, 'getThread').resolves(new Thread({id: 3, Title: 'some thread'}));
 			sandbox.stub(Post, 'addPost').resolves([10]);
 			const expected = {
 				id: 10
 			};
-			
+
 			return postController.addPost(mockRequest, mockResponse).then(() => mockResponse.send.should.have.been.calledWith(expected));
 		});
 	});
-	
+
 	describe('GET /posts/{ID}', () => {
 		before(() => {
 			mockRequest = {
@@ -104,25 +122,25 @@ describe('Post API Controller', () => {
 				}
 			};
 		});
-		
+
 		it('Should return 404 if no such post', () => {
 			sandbox.stub(Post, 'getPostByID').resolves(null);
 			return postController.getPost(mockRequest, mockResponse).then(() => mockResponse.status.should.have.been.calledWith(404));
 		});
-		
+
 		it('Should return posts if they exist', () => {
 			let postData = {
 				ID: 1,
 				Body: 'Somewhere over the rainbow?'
 			};
-			
+
 			const post = new Post(postData);
 			postData = post.serialize(); //add derived attributes
-			
+
 			sandbox.stub(Post, 'getPostByID').resolves(post);
 
-			
-			
+
+
 			return postController.getPost(mockRequest, mockResponse).then(() => {
 				Post.getPostByID.should.have.been.calledWith(100);
 
