1eea43ad8db9e67f75c1e07e6615939097cdec0c
Accalia <accalia@elementia.me>
2018-07-10 12:32:24 -0400

    chore(organization): Reorganize Board tests.
    This allows better use of code folding in editors to navigate tests
    accurately

--- test/unit/models/boardTests.js
+++ test/unit/models/boardTests.js
@@ -38,46 +38,61 @@ describe('Board model', () => {
 	});
 
 	const userID = 1;
-
-	it('should add a board', () => {
-		return Board.addBoard({
-			Owner: userID,
-			Name: 'Board1'
-		}).should.eventually.contain(1);
-	});
-
-	it('should set ID on add', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1'
+	describe('static addBoard()', () => {
+		it('should add a board', () => {
+			return Board.addBoard({
+				Owner: userID,
+				Name: 'Board1'
+			}).should.eventually.contain(1);
 		});
-		return Board.addBoard(board).then(() => {
-			board.ID.should.equal(1);
+
+		it('should set ID on add', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1'
+			});
+			return Board.addBoard(board).then(() => {
+				board.ID.should.equal(1);
+			});
+		});
+
+		it('should add a board object', () => {
+			return Board.addBoard(new Board({
+				Owner: userID,
+				Name: 'Board1'
+			})).should.eventually.contain(1);
+		});
+
+		it('should add a second board', () => {
+			return Board.addBoard({
+				Owner: userID,
+				Name: 'Boad1'
+			}).then(() => Board.addBoard({
+				Owner: userID,
+				Name: 'Board2'
+			})).should.eventually.contain(2);
+		});
+
+		it('should reject missing required fields', () => {
+			return Board.addBoard({}).should.be.rejectedWith(Error);
+		});
+
+		it('should not add a board with a game ID', () => {
+			return Board.addBoard({
+				Name: 'GameByID',
+				GameID: 1
+			}).should.be.rejectedWith(Error);
+		});
+
+		it('should not add a board with a game object', () => {
+			return Board.addBoard({
+				Name: 'GameByObject',
+				Game: {}
+			}).should.be.rejectedWith(Error);
 		});
 	});
 
-	it('should add a board object', () => {
-		return Board.addBoard(new Board({
-			Owner: userID,
-			Name: 'Board1'
-		})).should.eventually.contain(1);
-	});
-
-	it('should add a second board', () => {
-		return Board.addBoard({
-			Owner: userID,
-			Name: 'Boad1'
-		}).then(() => Board.addBoard({
-			Owner: userID,
-			Name: 'Board2'
-		})).should.eventually.contain(2);
-	});
-
-	it('should reject missing required fields', () => {
-		return Board.addBoard({}).should.be.rejectedWith(Error);
-	});
-
-	describe('static getBoards()', () => {
+	describe('static getChildrenOf()', () => {
 		it('should get boards and games', () => {
 			const expected = [1, 2, 3];
 			sandbox.stub(utils, 'getBoardsAndGames').resolves(expected);
@@ -88,132 +103,67 @@ describe('Board model', () => {
 		});
 	});
 
-	it('should find an existing board by ID', () => {
-		const boardObj = {
-			Owner: userID,
-			Name: 'Board1'
-		};
-		return Board.addBoard(boardObj)
-			.then(() => Board.get(1))
-			.should.eventually.contain.all({
-				ID: 1
-			});
-	});
-
-	it('should get all boards', () => {
-		const makers = [
-			Board.addBoard({
+	describe('static get()', () => {
+		it('should find an existing board by ID', () => {
+			const boardObj = {
 				Owner: userID,
 				Name: 'Board1'
-			}), Board.addBoard({
-				Owner: userID,
-				Name: 'Board2'
-			})
-		];
-		return Promise.all(makers)
-			.then(() => Board.getAllBoards())
-			.then((boards) => {
-				boards.should.have.length(2);
-				boards.filter((board) => board.Name === 'Board1').should.have.length(1);
-				boards.filter((board) => board.Name === 'Board2').should.have.length(1);
-			});
+			};
+			return Board.addBoard(boardObj)
+				.then(() => Board.get(1))
+				.should.eventually.contain.all({
+					ID: 1
+				});
+		});
+	});
+
+	describe('static getAllBoards()', () => {
+		it('should get all boards', () => {
+			const makers = [
+				Board.addBoard({
+					Owner: userID,
+					Name: 'Board1'
+				}), Board.addBoard({
+					Owner: userID,
+					Name: 'Board2'
+				})
+			];
+			return Promise.all(makers)
+				.then(() => Board.getAllBoards())
+				.then((boards) => {
+					boards.should.have.length(2);
+					boards.filter((board) => board.Name === 'Board1').should.have.length(1);
+					boards.filter((board) => board.Name === 'Board2').should.have.length(1);
+				});
+		});
 	});
 
 	it('should not find a non-existant board by ID', () => {
 		return Board.get(0).should.eventually.equal(null);
 	});
 
-	it('should get parent with parent ID set', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ParentID: 42
+	describe('Constructor', () => {
+		it('should set data from rowdata', () => {
+			const expected = {};
+			const board = new Board(expected);
+			board.data.should.equal(expected);
 		});
-		const parent = {
-			ID: 666
-		};
-		sandbox.stub(utils, 'getBoardOrGame').resolves(parent);
-		return board.getParent().then((result) => {
-			result.should.equal(parent);
-			utils.getBoardOrGame.calledWith(42).should.be.true;
+		it('should type coerce ID', () => {
+			const board = new Board({
+				ID: '42'
+			});
+			board.data.ID.should.equal(42);
 		});
-	});
-
-	it('should get parent with parent ID not set', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1'
-		});
-		sandbox.stub(utils, 'getBoardOrGame').rejects(new Error());
-		return board.getParent().should.eventually.become(null);
-	});
-
-	it('should set parent Board', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 1701
-		});
-		const parent = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 8472
-		});
-		sandbox.stub(board, 'save').resolves();
-		return board.setParent(parent).then(() => {
-			board.data.ParentID.should.equal(parent.ID);
-			board.save.called.should.be.true;
-		});
-	});
-	it('should set parent Game', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 1701
-		});
-		const parent = new Game({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 8472
-		});
-		sandbox.stub(board, 'save').resolves();
-		return board.setParent(parent).then(() => {
-			board.data.ParentID.should.equal(parent.ID);
-			board.save.called.should.be.true;
-		});
-	});
-
-	it('should reject non board parent ', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 1701
-		});
-		return board.setParent('hi there!').should.eventually.be.rejectedWith('Parent must be a Board');
-	});
-
-	it('should accept Null parent ', () => {
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 1701
-		});
-		return board.setParent(null).then(() => {
-			chai.expect(board.data.ParentID).to.equal(null);
-		});
-	});
-
-	it('should get children boards and games', () => {
-		const expected = [1, 2, 3];
-		sandbox.stub(utils, 'getBoardsAndGames').resolves(expected);
-		const board = new Board({
-			Owner: userID,
-			Name: 'Board1',
-			ID: 1701
-		});
-		return board.getChildren().then((results) => {
-			results.should.equal(expected);
-			utils.getBoardsAndGames.calledWith(1701).should.be.true;
+		
+		it('should type coerce ID', () => {
+			const board1 = new Board({
+				Adult: 0
+			});
+			board1.data.Adult.should.equal(false);
+			const board2 = new Board({
+				Adult: 'false'
+			});
+			board2.data.Adult.should.equal(true);
 		});
 	});
 
@@ -319,23 +269,91 @@ describe('Board model', () => {
 		});
 	});
 
-	describe('that are really Games', () => {
-		it('should not add a board with a game ID', () => {
-			return Board.addBoard({
-				Name: 'GameByID',
-				GameID: 1
-			}).should.be.rejectedWith(Error);
+	describe('getParent()', () => {
+		it('should get parent with parent ID set', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ParentID: 42
+			});
+			const parent = {
+				ID: 666
+			};
+			sandbox.stub(utils, 'getBoardOrGame').resolves(parent);
+			return board.getParent().then((result) => {
+				result.should.equal(parent);
+				utils.getBoardOrGame.calledWith(42).should.be.true;
+			});
 		});
-
-		it('should not add a board with a game object', () => {
-			return Board.addBoard({
-				Name: 'GameByObject',
-				Game: {}
-			}).should.be.rejectedWith(Error);
+	
+		it('should get parent with parent ID not set', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1'
+			});
+			sandbox.stub(utils, 'getBoardOrGame').rejects(new Error());
+			return board.getParent().should.eventually.become(null);
 		});
 	});
 
-	describe('with threads', () => {
+	describe('setParent()', () => {
+		it('should set parent Board', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 1701
+			});
+			const parent = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 8472
+			});
+			sandbox.stub(board, 'save').resolves();
+			return board.setParent(parent).then(() => {
+				board.data.ParentID.should.equal(parent.ID);
+				board.save.called.should.be.true;
+			});
+		});
+		it('should set parent Game', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 1701
+			});
+			const parent = new Game({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 8472
+			});
+			sandbox.stub(board, 'save').resolves();
+			return board.setParent(parent).then(() => {
+				board.data.ParentID.should.equal(parent.ID);
+				board.save.called.should.be.true;
+			});
+		});
+	
+		it('should reject non board parent ', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 1701
+			});
+			return board.setParent('hi there!').should.eventually.be.rejectedWith('Parent must be a Board');
+		});
+	
+		it('should accept Null parent ', () => {
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 1701
+			});
+			return board.setParent(null).then(() => {
+				chai.expect(board.data.ParentID).to.equal(null);
+			});
+		});
+	});
+
+	describe('getThreads()', () => {
 		let board;
 		const Thread = require('../../../src/model/Thread.js');
 
@@ -373,6 +391,22 @@ describe('Board model', () => {
 				Title: 'A Thread'
 			}]);
 			return board.getThreads().should.eventually.deep.equal([1]);
+		});
+	});
+
+	describe('getChildren()', () => {
+		it('should get children boards and games', () => {
+			const expected = [1, 2, 3];
+			sandbox.stub(utils, 'getBoardsAndGames').resolves(expected);
+			const board = new Board({
+				Owner: userID,
+				Name: 'Board1',
+				ID: 1701
+			});
+			return board.getChildren().then((results) => {
+				results.should.equal(expected);
+				utils.getBoardsAndGames.calledWith(1701).should.be.true;
+			});
 		});
 	});
 });
